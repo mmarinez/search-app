7afcb8b24bfe6cc40b613fdffaf66334a586d834
Manuel Marinez <marinezdev@gmail.com>
2020-01-19 23:34:40 -0400

    Add search functionality

--- program.py
+++ program.py
@@ -4,8 +4,19 @@ import os
 def main():
     search_app_welcome()
     folder = get_user_folder()
+    if not folder:
+        print("Sorry we can't search that location")
+        return
+
     text = get_text_from_user()
-    search_folders(folder, text)
+    if not text:
+        print("Sorry we can't search for nothing!")
+        return
+
+    matches = search_folders(folder, text)
+
+    for match in matches:
+        print(match)
 
 
 def search_app_welcome():
@@ -31,7 +42,29 @@ def get_text_from_user():
 
 
 def search_folders(folder, text):
-    print("Would search {} for {}".format(folder, text))
+    all_matches = []
+    items = os.listdir(folder)
+
+    for item in items:
+        full_item = os.path.join(folder, item)
+        if os.path.isdir(full_item):
+            continue
+
+        matches = search_file(full_item, text)
+        all_matches.extend(matches)
+
+    return all_matches
+
+
+def search_file(filename, search_text):
+    matches = []
+    with open(filename, 'r', encoding='utf-8') as fin:
+
+        for line in fin:
+            if line.lower().find(search_text) >= 0:
+                matches.append(line)
+
+        return matches
 
 
 if __name__ == "__main__":
